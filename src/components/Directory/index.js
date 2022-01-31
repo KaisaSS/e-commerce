96988a7c77ab47d001e1ce75d717e0b0a6c5c384
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2022-01-31 11:51:33 +0000

    Create Directory component

---
+++ src/components/Directory/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ShopMen from "../../assets/shopMens.jpg";
+import ShopWomen from "../../assets/shopWomens.jpg";
+import "./styles.scss";
+
+const Directory = () => {
+  return (
+    <div className="directory">
+      <div className="wrap">
+        <div className="item" style={{ backgroundImage: `url(${ShopWomen})` }}>
+          <a href="">Shop Women's</a>
+        </div>
+        <div className="item" style={{ backgroundImage: `url(${ShopMen})` }}>
+          <a href="">Shop Men's</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Directory;
