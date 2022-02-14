18b69eb275b47548436fce91ffc192b9364bdf24
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2022-02-14 16:00:17 +0000

    Set search filters

--- src/components/ProductResults/index.js
+++ src/components/ProductResults/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
 import { fetchProductsStart } from "../../redux/Products/products.actions";
 import Product from "./Product";
+import FormSelect from "../forms/FormSelect";
 import "./styles.scss";
 
 const mapState = ({ productsData }) => ({
@@ -10,11 +12,19 @@ const mapState = ({ productsData }) => ({
 
 const ProductResults = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
+  const { filterType } = useParams();
   const { products } = useSelector(mapState);
 
   useEffect(() => {
-    dispatch(fetchProductsStart());
-  }, []);
+    dispatch(fetchProductsStart({ filterType }));
+    console.log(filterType);
+  }, [filterType]);
+
+  const handleFilter = (e) => {
+    const nextFilter = e.target.value;
+    history.push(`/search/${nextFilter}`);
+  };
 
   if (!Array.isArray(products)) return null;
 
@@ -26,9 +36,29 @@ const ProductResults = () => {
     );
   }
 
+  const configFilters = {
+    defaultValue: filterType,
+    options: [
+      {
+        name: "Show all",
+        value: "",
+      },
+      {
+        name: "Men's",
+        value: "mens",
+      },
+      {
+        name: "Women's",
+        value: "womens",
+      },
+    ],
+    handleChange: handleFilter,
+  };
+
   return (
     <div className="products">
       <h1>Search products</h1>
+      <FormSelect {...configFilters} />
 
       <div className="productResults">
         {products.map((product, i) => {
